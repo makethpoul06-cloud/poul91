6ddcab2fd6a651affb11fa8de00c6921a26c8256
khalid <khalidkamalabdo@gmail.com>
2026-06-19 12:09:12 +0300

    change home page into index

---
+++ logic.js
@@ -0,0 +1,38 @@
+//function addnumber(num1, num2) {
+    //const sum =num1 + num2;
+    //return sum;
+//}
+
+//const result = addnumber(5, 10);//function call
+//print the result
+//console.log(result);
+
+//let length = 10;
+//let width = 5;
+//let area = length * width;
+//console.log(area);
+//const result = area;
+
+
+//for loop
+//for ( num =1;num<=100;num++){
+//  //console.log(num);
+//}
+
+
+
+
+//while loop
+  let num =100
+  while (num >0){
+    console.log(num);
+    num--;
+} 
+
+
+//do while loop
+do{
+    console.log(num);
+    num--
+}
+while (num > 0);
